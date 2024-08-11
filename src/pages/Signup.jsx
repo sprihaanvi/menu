3e32d3384030ff55e37af6a584687b34a8d59b27
Spriha Anvi <90754743+sprihaanvi@users.noreply.github.com>
2024-08-11 12:34:02 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,7 +13,7 @@ export default function Signup() {
   });
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://menu-backend-86pz.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
